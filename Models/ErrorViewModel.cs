86a5b840c02d33a8cd23b863988efdb0f4a65809
Elvin Medzidov <medzidov18.03@gmail.com>
2022-07-11 12:43:03 +0400

    Info fixed
    111

--- Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace OnlineBooks.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
