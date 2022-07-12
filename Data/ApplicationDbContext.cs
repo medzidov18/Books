d06f16f8cb7090bda3316324c60e0a45cf4248f0
Elvin Medzidov <medzidov18.03@gmail.com>
2022-07-12 21:25:53 +0400

    Books
    added .gitignore for safety

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -21,35 +21,7 @@ namespace OnlineBooks.Data
         public DbSet<Books> AllBooks { get; set; }
 
 
-        protected override void OnModelCreating(ModelBuilder modelBuilder)
-        {
-            base.OnModelCreating(modelBuilder);
-
-            modelBuilder.Entity<IdentityRole>().HasData(new IdentityRole
-            {
-                Id = "44546e06-8719-4ad8-b88a-f271ae9d6eab",
-                Name = "admin",
-                NormalizedName = "ADMIN"
-            });
-
-            modelBuilder.Entity<IdentityUser>().HasData(new IdentityUser
-            {
-                Id = "3b62472e-4f66-49fa-a20f-e7685b9565d8",
-                UserName = "admin",
-                NormalizedUserName = "ADMIN",
-                Email = "my@email.com",
-                NormalizedEmail = "MY@EMAIL.COM",
-                EmailConfirmed = true,
-                PasswordHash = new PasswordHasher<IdentityUser>().HashPassword(null, "superpassword"),
-                SecurityStamp = string.Empty
-            });
-
-            modelBuilder.Entity<IdentityUserRole<string>>().HasData(new IdentityUserRole<string>
-            {
-                RoleId = "44546e06-8719-4ad8-b88a-f271ae9d6eab",
-                UserId = "3b62472e-4f66-49fa-a20f-e7685b9565d8"
-            });
-        }
+        
     }
 
 }
